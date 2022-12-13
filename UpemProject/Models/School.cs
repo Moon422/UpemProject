6d4f162e901222eb9e47f93f62b0b1ec58d8b795
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-14 00:57:38 +0600

    model cleanup

--- UpemProject/Models/School.cs
+++ UpemProject/Models/School.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,4 +15,6 @@ public class School
 
     [Required]
     public string Code { get; set; }
+
+    public List<Department> Departments { get; set; }
 }
