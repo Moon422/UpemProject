0c31b9e1331bfe50b9435bc90afb1ba539a6e0af
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-14 16:45:06 +0600

    string length fixed

--- UpemProject/Models/User.cs
+++ UpemProject/Models/User.cs
@@ -10,12 +10,15 @@ public abstract class User
     public Guid Id { get; set; }
 
     [Required]
+    [MaxLength(10)]
     public string OrganizationId { get; set; }
 
     [Required]
+    [MaxLength(100)]
     public string FirstName { get; set; }
 
     [Required]
+    [MaxLength(50)]
     public string LastName { get; set; }
 
     [Required]
