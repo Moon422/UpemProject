d82d23ac6287e564deda457a754800008cb85c90
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-20 20:09:36 +0600

    migration 2

---
+++ Backend/Migrations/20221220135620_migration_2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    public partial class migration_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Departments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Departments",
+                type: "varchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
