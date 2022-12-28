bcd2b66dcfc26151e2d4403ff878e504dbad23c7
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2022-12-29 01:15:46 +0600

    migration 3

--- Backend/Migrations/20221228191433_migration3.Designer.cs
+++ Backend/Migrations/20221228191433_migration3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(UpemDbContext))]
-    partial class UpemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221228191433_migration3")]
+    partial class migration3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
